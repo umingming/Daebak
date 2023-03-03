7c2ea30fe3612b4dce2d686b164aed04ecbb31a0
umingming <u_0112@naver.com>
2023-03-03 17:00:28 +0900

    feat: Apply datepicker

--- daebak/src/views/BoardView.vue
+++ daebak/src/views/BoardView.vue
@@ -1,12 +1,12 @@
 <template>
     <div class="board">
-        <modal-add 
+        <modal-add
             v-if="isShowModal.add"
             :checkItem="checkItems[0]"
             @close="isShowModal['add'] = false"
         >
         </modal-add>
-        <modal-modify 
+        <modal-modify
             v-if="isShowModal.modify"
             :checkItems="checkItems"
             @close="isShowModal['modify'] = false"
@@ -14,40 +14,30 @@
         </modal-modify>
         <div class="board-box">
             <div class="search">
-                <input type="date">
-                <input type="date">
-                <select 
-                    name="type"
-                >
+                <date-picker
+                    v-model="searchDate"
+                    value-type="format"
+                    range
+                ></date-picker>
+                <select name="type">
                     <option value="title">내용</option>
                     <option value="value">금액</option>
                     <option value="amount">주문</option>
                     <option value="cate">분류</option>
                 </select>
-                <input type="text" id="keyword">
+                <input type="text" id="keyword" />
             </div>
             <div class="btn-icon">
                 <button class="btn-refresh">
                     <i class="fa-solid fa-arrow-rotate-right"></i>
                 </button>
-                <button 
-                    class="btn-add"
-                    id="add"
-                    @click="showModal"
-                >
+                <button class="btn-add" id="add" @click="showModal">
                     <i class="fa-solid fa-square-plus"></i>
                 </button>
-                <button 
-                    class="btn-modify"
-                    id="modify"
-                    @click="showModal"
-                >
+                <button class="btn-modify" id="modify" @click="showModal">
                     <i class="fa-solid fa-pen"></i>
                 </button>
-                <button 
-                    class="btn-delete"
-                    id="delete"
-                >
+                <button class="btn-delete" id="delete">
                     <i class="fa-solid fa-trash"></i>
                 </button>
                 <export-excel
@@ -74,6 +64,8 @@
 import ModalAdd from "@/components/BudgetModalAdd.vue";
 import ModalModify from "@/components/BudgetModalModify.vue";
 import BudgetTable from "@/components/BudgetTable.vue";
+import DatePicker from "vue2-datepicker";
+import "vue2-datepicker/index.css";
 
 import { mapGetters } from "vuex";
 
@@ -81,7 +73,8 @@ export default {
     components: {
         ModalAdd,
         ModalModify,
-        BudgetTable
+        BudgetTable,
+        DatePicker,
     },
     data() {
         return {
@@ -90,42 +83,43 @@ export default {
                 내용: "title",
                 금액: "value",
                 주문: "amount",
-                분류: "cate"
+                분류: "cate",
             },
             checkList: [],
             isShowModal: {
                 add: false,
-                modify: false
+                modify: false,
             },
             addItem: {
                 date: "",
                 title: "",
                 value: "",
                 amount: "",
-                cate: ""
-            }
-        }
+                cate: "",
+            },
+            searchDate: null,
+        };
     },
     computed: {
         ...mapGetters(["fetchedList"]),
         checkItems() {
             return [...this.checkList];
-        }
+        },
     },
     created() {
         this.$store.dispatch("FETCH_LIST");
     },
     methods: {
-        showModal({target}) {
-            this.checkList = this.fetchedList.filter(i => i.check);
+        showModal({ target }) {
+            this.checkList = this.fetchedList.filter((i) => i.check);
 
             let key = target.id || target.parentNode.id;
             this.isShowModal[key] = true;
         },
         checkItem(index, isChecked) {
             this.fetchedList[index].check = isChecked;
-        }
-    }
+        },
+    },
 };
 </script>
 
@@ -151,5 +145,59 @@ export default {
 }
 .board-box .search #keyword {
     width: 120px;
+}
+</style>
+<style lang="scss">
+.mx-datepicker {
+    width: 205px !important;
+    .mx-input {
+        width: 205px !important;
+    }
+    margin-right: 10px;
+}
+.mx-icon-calendar {
+    color: #ff7b00;
+}
+.mx-icon-clear,
+.mx-btn {
+    &:hover {
+        color: #ff7b00;
+    }
+}
+.mx-calendar-content {
+    .cell {
+        &:hover {
+            color: #ff7b00;
+            background: #ff7b000b;;
+        }
+        &:active {
+            background: #ff7b0079;
+            color: white;
+        }
+        &.in-range,
+        &.hover-in-range {
+            background: #ff7b0014;
+        }
+    }
+}
+.mx-table-date {
+    .today {
+        color: #ff7b00;
+    }
+    .mx-date-row {
+        .cell {
+            &.active {
+                background: #ff7b0079;
+            }
+        }
+    }
+}
+.mx-table-year,
+.mx-table-month { 
+    .cell {
+        &.active {
+            background: #ff7b0079;
+        }
+    }
 }
 </style>
