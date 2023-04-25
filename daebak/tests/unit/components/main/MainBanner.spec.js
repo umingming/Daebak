af19ba0435ec47894e4cc96ae6273f75f12f7869
umingming <u_0112@naver.com>
2023-04-25 09:15:53 +0900

    test

--- daebak/tests/unit/components/main/MainBanner.spec.js
+++ daebak/tests/unit/components/main/MainBanner.spec.js
@@ -3,48 +3,53 @@ import Vuex from "vuex";
 import MainBanner from "@/components/main/MainBanner.vue";
 import { BANNER } from "@/constants/main.js";
 
-const localVue = createLocalVue();
-localVue.use(Vuex);
-const store = new Vuex.Store({
-    modules: {
-        order: {
-            getters: {
-                currentMonthValues() {
-                    return jest.fn();
-                },
-            },
-            namespaced: true,
-        },
-    },
-});
-const month = (new Date().getMonth() + 1).toString();
+describe("MainBanner", () => {
+    const localVue = createLocalVue();
+    localVue.use(Vuex);
 
-describe.each`
-    title      | expected
-    ${"value"} | ${`${month}월 총 ${BANNER["value"]}`}
-    ${"order"} | ${`${month}월 총 ${BANNER["order"]}`}
-`("MainBanner Title", ({ title, expected }) => {
-    test(`renders ${title} title by correct month`, () => {
-        //Given
+    const store = new Vuex.Store({
+        modules: {
+            order: {
+                getters: {
+                    currentMonthValues() {
+                        return jest.fn();
+                    },
+                },
+                namespaced: true,
+            },
+        },
+    });
+
+    const month = (new Date().getMonth() + 1).toString();
+
+    describe.each(
+        Object.entries(BANNER).map(([key, value]) => ({
+            type: key,
+            expected: `${month}월 총 ${value}`,
+        }))
+    )("Title: %s", ({ type, expected }) => {
+        test(`renders "${type}" title by correct month`, () => {
+            // Given
+            const wrapper = mount(MainBanner, {
+                localVue,
+                propsData: { month, type },
+                store,
+            });
+
+            // Then
+            expect(wrapper.find('[data-test="title"]').text()).toBe(expected);
+        });
+    });
+
+    test("renders the value increment correctly", () => {
+        // Given
         const wrapper = mount(MainBanner, {
             localVue,
-            propsData: { month, type: title },
             store,
         });
 
-        //Then
-        expect(wrapper.find('[data-test="title"]').text()).toBe(expected);
+        // Then
+        const increment = wrapper.find('[data-test="increment"]').text();
+        expect(isNaN(increment.replace(",", ""))).toBe(false);
     });
-});
-
-test("renders the value increment correctly", () => {
-    //Given
-    const wrapper = mount(MainBanner, {
-        localVue,
-        store,
-    });
-
-    //Then
-    const increment = wrapper.find('[data-test="increment"]').text();
-    expect(isNaN(increment.replace(",", ""))).toBe(false);
 });
